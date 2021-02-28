0486bd2a97803df6d3cce70a00ea6e316cf50b24
Amrita Mind Brain Center <79529753+amritamindbrain@users.noreply.github.com>
2021-02-28 11:41:40 +0530

    Update pretest.js

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -89,23 +89,36 @@
 
 
      const myQuestions = [{
-            question: "1.This is a sample question:", ///// Write the question inside double quotes
+            question: "1.Neuroimaging techniques used for bio signal acquisition are:", ///// Write the question inside double quotes
             answers: {
-                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
-                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
+                a: "EEG", ///// Write the option 1 inside double quotes
+                b: "fmri",
+                c: "EMG",
+                d: "All the above",///// Write the option 2 inside double quotes
             },
-            correctAnswer: "a" ///// Write the correct option inside double quotes
+            correctAnswer: "d" ///// Write the correct option inside double quotes
         },
 
     {
-      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
-      answers: {
-        a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
-        b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
-        c: "<img src='images/1c.PNG'>",      },
-      correctAnswer: "c"                ///// Write the correct option inside double quotes
-    },
-
+            question: "2.Electroencephalographic data are recorded in which format:", ///// Write the question inside double quotes
+            answers: {
+                a: "As PDF File", ///// Write the option 1 inside double quotes
+                b: "As EDF file",
+                c: "As word file",
+                d: "None of the above",///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "b" ///// Write the correct option inside double quotes
+        },
+{
+            question: "3.Clinically EEG technique is applied to detect:", ///// Write the question inside double quotes
+            answers: {
+                a: "Encephalopathy", ///// Write the option 1 inside double quotes
+                b: "Encephalitis",
+                c: "Brain death",
+                d: "All the above.",///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "d" ///// Write the correct option inside double quotes
+        },
 
 
      ];
